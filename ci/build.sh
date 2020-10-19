24e474138cdb93c5606990897dfda52c0bc203aa
Benjamin Fischer <benjamin.fischer@evolutics.info>
2020-10-19 21:08:35 +0200

    Add script as entry point to continuous integration

---
+++ ci/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+main() {
+  local -r script_folder="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  local -r project_folder="$(dirname "${script_folder}")"
+
+  pushd "${project_folder}"
+
+  ls
+
+  popd
+}
+
+main "$@"
