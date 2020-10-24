5a61c55eb2fec51a2c38d454354b4d9415c91813
Benjamin Fischer <benjamin.fischer@evolutics.info>
2020-10-24 12:11:24 +0200

    Model code cleaners as data for regularity

--- src/main.py
+++ src/main.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python3
 
 import argparse
+import dataclasses
 import subprocess
 import sys
+import typing
 
 
 def main():
+    cleaners = _cleaners()
+
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
     subparser = subparsers.add_parser("check")
     subparser.set_defaults(
         calls=[
-            _black_check,
-            _git_check,
-            _gitlint_check,
-            _hadolint_check,
-            _hunspell_check,
-            _prettier_check,
+            cleaner.check for cleaner in cleaners.values() if cleaner.check is not None
         ]
     )
     subparser = subparsers.add_parser("fix")
     subparser.set_defaults(
-        calls=[
-            _black_fix,
-            _prettier_fix,
-        ]
+        calls=[cleaner.fix for cleaner in cleaners.values() if cleaner.fix is not None]
     )
 
     arguments = parser.parse_args()
@@ -33,74 +29,72 @@ def main():
     exit_status = 0
     for call in arguments.calls:
         try:
-            subprocess.run(call(), check=True, shell=True)
+            subprocess.run(call, check=True, shell=True)
         except subprocess.CalledProcessError:
             exit_status = 1
     sys.exit(exit_status)
 
 
-def _black_check():
-    return "git ls-files -z -- '*.py' '*.pyi' | xargs -0 black --check --diff"
+@dataclasses.dataclass
+class _Cleaner:
+    check: typing.Optional[str]
+    fix: typing.Optional[str]
 
 
-def _black_fix():
-    return "git ls-files -z -- '*.py' '*.pyi' | xargs -0 black"
-
-
-def _git_check():
-    return "git diff --check HEAD^"
-
-
-def _gitlint_check():
-    return "gitlint --ignore body-is-missing"
-
-
-def _hadolint_check():
-    return (
-        "git ls-files -z -- "
-        "'*.Dockerfile' "
-        "'*/Dockerfile' "
-        "Dockerfile "
-        "| xargs -0 hadolint"
-    )
-
-
-def _hunspell_check():
-    return (
-        "git log -1 --format=%B | hunspell -l -d en_US -p ci/personal_words.dic "
-        "| sort | uniq | tr '\\n' '\\0' | xargs -0 -r -n 1 sh -c "
-        """'echo "Misspelling: $@"; exit 1' --"""
-    )
-
-
-def _prettier_check():
-    return (
-        "git ls-files -z -- "
-        "'*.css' "
-        "'*.html' "
-        "'*.js' "
-        "'*.json' "
-        "'*.md' "
-        "'*.ts' "
-        "'*.yaml' "
-        "'*.yml' "
-        "| xargs -0 prettier --check"
-    )
-
-
-def _prettier_fix():
-    return (
-        "git ls-files -z -- "
-        "'*.css' "
-        "'*.html' "
-        "'*.js' "
-        "'*.json' "
-        "'*.md' "
-        "'*.ts' "
-        "'*.yaml' "
-        "'*.yml' "
-        "| xargs -0 prettier --write"
-    )
+def _cleaners():
+    return {
+        "Black": _Cleaner(
+            check="git ls-files -z -- '*.py' '*.pyi' | xargs -0 black --check --diff",
+            fix="git ls-files -z -- '*.py' '*.pyi' | xargs -0 black",
+        ),
+        "Git": _Cleaner(check="git diff --check HEAD^", fix=None),
+        "Gitlint": _Cleaner(check="gitlint --ignore body-is-missing", fix=None),
+        "Haskell Dockerfile Linter": _Cleaner(
+            check=(
+                "git ls-files -z -- "
+                "'*.Dockerfile' "
+                "'*/Dockerfile' "
+                "Dockerfile "
+                "| xargs -0 hadolint"
+            ),
+            fix=None,
+        ),
+        "Hunspell": _Cleaner(
+            check=(
+                "git log -1 --format=%B "
+                "| hunspell -l -d en_US -p ci/personal_words.dic "
+                "| sort | uniq | tr '\\n' '\\0' | xargs -0 -r -n 1 sh -c "
+                """'echo "Misspelling: $@"; exit 1' --"""
+            ),
+            fix=None,
+        ),
+        "Prettier": _Cleaner(
+            check=(
+                "git ls-files -z -- "
+                "'*.css' "
+                "'*.html' "
+                "'*.js' "
+                "'*.json' "
+                "'*.md' "
+                "'*.ts' "
+                "'*.yaml' "
+                "'*.yml' "
+                "| xargs -0 prettier --check"
+            ),
+            fix=(
+                "git ls-files -z -- "
+                "'*.css' "
+                "'*.html' "
+                "'*.js' "
+                "'*.json' "
+                "'*.md' "
+                "'*.ts' "
+                "'*.yaml' "
+                "'*.yml' "
+                "| xargs -0 prettier --write"
+            ),
+        ),
+    }
 
 
 if __name__ == "__main__":
