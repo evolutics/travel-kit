b98d78e577e0bb73f7864ea6b151afb7d1785170
Benjamin Fischer <benjamin.fischer@evolutics.info>
2020-11-07 11:20:54 +0100

    Add script to automate release process

---
+++ scripts/release.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("version")
+    arguments = parser.parse_args()
+    version = arguments.version
+
+    _check_that_head_is_current()
+    _tag_head(version)
+
+
+def _check_that_head_is_current():
+    subprocess.run(["git", "fetch"], check=True)
+    subprocess.run(["git", "diff", "--exit-code", "HEAD", "origin/main"], check=True)
+
+
+def _tag_head(version):
+    subprocess.run(
+        ["git", "tag", "--annotate", version, "--message", version], check=True
+    )
+    subprocess.run(["git", "push", "origin", version], check=True)
+
+
+if __name__ == "__main__":
+    main()
