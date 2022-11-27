68ec666b55c1278c6378740f4cead41b336bfd83
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-11-27 12:47:05 +0100

    Discover packages automatically

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(
     entry_points={"console_scripts": ["travel-kit=travelkit.main:main"]},
     name="travelkit",
     package_data={"travelkit": ["cleaners.json"]},
-    packages=["travelkit"],
+    packages=setuptools.find_packages(),
 )
