89a0dbb61fc0bb554852d765d38dc36938040fc2
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-02-04 11:13:32 +0100

    Fix flake to make it work with more up-to-date Nix version
    Error message:
    
    ```
    error: attribute 'defaultApp.x86_64-linux' should have type 'app'
    ```
    
    Fix with help of https://github.com/NixOS/nix/issues/6448.

--- flake.nix
+++ flake.nix
@@ -146,16 +146,18 @@
           };
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        defaultApp = pkgs.python3Packages.buildPythonApplication {
-          format = "pyproject";
-          name = "travel-kit";
-          preBuild = ''
-            cat >travelkit/cleaners.json <<'EOF'
-            ${cleaners}
-            EOF
-          '';
-          propagatedBuildInputs = [pkgs.python3Packages.setuptools];
-          src = ./.;
+        apps.default = flake-utils.lib.mkApp {
+          drv = pkgs.python3Packages.buildPythonApplication {
+            format = "pyproject";
+            name = "travel-kit";
+            preBuild = ''
+              cat >travelkit/cleaners.json <<'EOF'
+              ${cleaners}
+              EOF
+            '';
+            propagatedBuildInputs = [pkgs.python3Packages.setuptools];
+            src = ./.;
+          };
         };
       }
     );
