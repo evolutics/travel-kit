7b1268370f489f7507bf883f7ed771a5c4a6217d
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-02-07 20:29:39 +0100

    Extract functions for clarity

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,24 +4,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-main() {
-  local -r script_folder="$(dirname "$(readlink --canonicalize "$0")")"
-  cd "$(dirname "${script_folder}")"
-
+check_basics() {
   nix flake check
-
   git ls-files -z | xargs -0 nix run . -- check --
-
   nix run . -- readme | diff README.md -
+}
 
-  git rm --force flake.lock
-
+sanity_check_example_integration() {
   (
     cd example
     nix flake check
     nix develop . --command travel-kit --help
     git rm --force flake.lock
   )
+}
+
+main() {
+  local -r script_folder="$(dirname "$(readlink --canonicalize "$0")")"
+  cd "$(dirname "${script_folder}")"
+
+  check_basics
+  git rm --force flake.lock
+
+  sanity_check_example_integration
 }
 
 main "$@"
