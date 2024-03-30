76e694043989b38f94b2dd20faab2767e99dc95d
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-03-30 17:42:07 +0100

    Test cleaners with real run to be confident about correct integration

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,41 +6,13 @@ set -o pipefail
 
 check_basics() {
   nix flake check
-  git ls-files -z | xargs -0 nix run . -- check --
+  git ls-files -z -- ':!:test/cases/' | xargs -0 nix run . -- check --
   nix run . -- readme | diff README.md -
-}
-
-test_cleaner_sample_runs() {
-  echo '{"hi" :5 }' >sample.json
-  nix run . -- check sample.json && exit 1
-  nix run . -- fix sample.json
-  nix run . -- check sample.json
-  rm sample.json
 }
 
 test_cleaner_command_constructions() {
   (
     cd test/cases
-    rm --force -- *
-
-    touch \
-      alejandra.nix \
-      black.py \
-      git \
-      gitlint \
-      hadolint.Dockerfile \
-      html5validator.htm \
-      htmlhint.htm \
-      isort.py \
-      jsonnet_lint.jsonnet \
-      jsonnetfmt.jsonnet \
-      prettier.js \
-      pylint.py \
-      rufo.rb \
-      shellcheck.sh \
-      shfmt.sh \
-      stylelint.css \
-      terraform.tf
 
     for subcommand in check fix; do
       nix run ../.. -- "${subcommand}" --dry-run -- * \
@@ -48,6 +20,47 @@ test_cleaner_command_constructions() {
         | diff "../expected/${subcommand}.txt" -
     done
   )
+}
+
+test_cleaners() {
+  local -r status="$(git status --porcelain test/cases)"
+  if [[ -n "${status}" ]]; then
+    >&2 echo "${status}"
+    exit 1
+  fi
+
+  local actual_output
+  actual_output="$(nix run . check test/cases/* 2>&1)" && exit 1
+  readonly actual_output
+
+  for lint in test/lints/*; do
+    readarray -t expected_lines < <(cat "${lint}")
+    for expected_line in "${expected_lines[@]}"; do
+      if ! echo "${actual_output}" \
+        | grep --fixed-strings --quiet "${expected_line}"; then
+        >&2 echo "${lint}: ${expected_line}"
+        exit 1
+      fi
+    done
+  done
+
+  nix run . fix test/cases/*
+
+  local -r delta="$(diff --recursive test/fixed test/cases)"
+  if [[ "${delta}" != "Only in test/cases: git
+Only in test/cases: gitlint
+Only in test/cases: hadolint.Dockerfile
+Only in test/cases: html5validator.htm
+Only in test/cases: htmlhint.htm
+Only in test/cases: jsonnet_lint.jsonnet
+Only in test/cases: pylint.py
+Only in test/cases: shellcheck.sh
+Only in test/cases: stylelint.css" ]]; then
+    >&2 echo "${delta}"
+    exit 1
+  fi
+
+  git restore test/cases
 }
 
 sanity_check_example_integration() {
@@ -64,8 +77,8 @@ main() {
   cd "$(dirname "${script_folder}")"
 
   check_basics
-  test_cleaner_sample_runs
   test_cleaner_command_constructions
+  test_cleaners
   git rm --force flake.lock
 
   sanity_check_example_integration
---
+++ test/cases/isort.py
@@ -0,0 +1,4 @@
+import sys
+import os
+
+print(os.name, file=sys.stderr)
