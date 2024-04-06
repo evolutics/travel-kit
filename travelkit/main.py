6e439951e3081c08564b037ef7a94c57b197b444
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-04-06 15:16:17 +0200

    Merge into main for simplicity

--- travelkit/main.py
+++ travelkit/main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
+from importlib import resources
 import argparse
+import json
 
-from . import cleaners
+from . import model
 from . import readme
 from . import run_cleaners
 
 
 def main():
-    cleaners_ = cleaners.get()
+    cleaners = _get_cleaners()
 
     parser = argparse.ArgumentParser()
 
@@ -17,7 +19,7 @@ def main():
     parser.add_argument(
         "--skip",
         action="append",
-        choices=cleaners_,
+        choices=cleaners,
         default=[],
         help="Do not use this tool.",
     )
@@ -26,17 +28,34 @@ def main():
     arguments = parser.parse_args()
 
     if arguments.readme:
-        print(readme.get(cleaners_))
+        print(readme.get(cleaners))
         return
 
     run_cleaners.get(
         cleaners={
             identifier: cleaner
-            for identifier, cleaner in cleaners_.items()
+            for identifier, cleaner in cleaners.items()
             if identifier not in arguments.skip
         },
         is_dry_run=arguments.dry_run,
         file_paths=arguments.file_paths,
+    )
+
+
+def _get_cleaners():
+    data_path = resources.files("travelkit").joinpath("cleaners.json")
+    with data_path.open("br") as data_file:
+        data = json.load(data_file)
+
+    return {identifier: _get_cleaner(cleaner) for identifier, cleaner in data.items()}
+
+
+def _get_cleaner(raw):
+    return model.Cleaner(
+        title=raw["title"],
+        homepage=raw["homepage"],
+        file_patterns=tuple(raw["file_patterns"]),
+        command=tuple(raw["command"]),
     )
 
 
